3ebe8ab9126d24507f8df297492e52e1044d862a
StuartLane925 <60291553+StuartLane925@users.noreply.github.com>
2025-01-08 15:45:04 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,35 +1,17 @@
 const express = require('express');
-const cors = require('cors');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Middleware
-app.use(cors());
-app.use(express.json());
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Variables
-let variables = {
-  Open: "Yes",
-  Emergency: "No",
-  CCBEnabled: "True"
-};
-
-// Endpoint to get variables
+// Endpoint to serve the JSON file
 app.get('/api/variables', (req, res) => {
-  res.json(variables);
+    res.sendFile(path.join(__dirname, 'public/variables.json'));
 });
 
-// Endpoint to update variables
-app.post('/api/variables', (req, res) => {
-  const { Open, Emergency, CCBEnabled } = req.body;
-  if (Open !== undefined) variables.Open = Open;
-  if (Emergency !== undefined) variables.Emergency = Emergency;
-  if (CCBEnabled !== undefined) variables.CCBEnabled = CCBEnabled;
-
-  res.json({ message: 'Variables updated successfully', variables });
-});
-
-// Start the server
 app.listen(port, () => {
-  console.log(`Server is running on https://wxcc.onrender.com:${port}`);
+    console.log(`Server running on https://wxcc.onrender.com:${port}`);
 });
+
